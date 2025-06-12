b0298f648b966131fd8ecb80c9ce5c235aab143b
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-06-12 10:33:46 +0700

    Menambahkan konstruktor untuk inisialisisasi nilai FRONT dan REAR ke -1

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -8,5 +8,75 @@ private:
     int FRONT, REAR;
     int queue_array[5];
 
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+    
+    // 1.Cek apakah antrian penuh
+    if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1))
+    {
+        cout << "\nQueue overflow\n"; // 1.A
+        return; // 1.B
+    }
+
+    // Cek apakah antrian kosong
+    if (FRONT == -1)
+    {
+        FRONT = 0; //2.A
+        REAR = 0; //2.B
+    }
+    else
+    {
+        // Jika REAR berada dinposisi terakhir array,kembali ke awal array
+        if (REAR == max - 1)
+            REAR = 0;
+        else 
+            REAR = REAR + 1;
+    }
+    queue_array[REAR] = num;
 }
 
+void remove()
+{
+    //Cek apakah antrian kosong
+    if(FRONT == -1)
+    {
+        cout << "Queue underflow\n";
+        return;
+    } 
+    cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+    //Cek jika antrian hanya memiliki satu elemen
+    if (FRONT == REAR)
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+    else
+    {
+        //Jika elemen yang dihapus berada di posisi terakhir array,kembali ke array awal
+        if (FRONT == max -1)
+            FRONT = 0;
+        else 
+            FRONT = FRONT + 1;    
+    } 
+}
+
+void display()
+{
+    int FRONT_position = FRONT;
+    int REAR_position = REAR;
+
+    //Cek apakah antrian kososng
+}
+};
